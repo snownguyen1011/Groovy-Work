7d0b5af90f44c08891fb43231d021dcb64a33a52
snownguyen1011 <raviraja1988@hotmail.com>
2020-11-05 19:53:33 -0500

    modified code to generate prop files

--- Filtering_Services/Jenkinsfile.groovy
+++ Filtering_Services/Jenkinsfile.groovy
@@ -17,41 +17,51 @@ pipeline {
                         def validServices = []
                         def invalidServices = []
                         def validSchedulers = []
-                        File file1 = new File("${WORKSPACE}/Filtering_Services/validServices.txt")
-                        File file2 = new File("${WORKSPACE}/Filtering_Services/invalidServices.txt")
-                        File file3 = new File("${WORKSPACE}/Filtering_Services/validSchedulers.txt")
+                        File file1 = new File("${WORKSPACE}/Filtering_Services/validServices.properties")
+                        File file2 = new File("${WORKSPACE}/Filtering_Services/invalidServices.properties")
+                        File file3 = new File("${WORKSPACE}/Filtering_Services/validServicesSchedulers.properties")
+                        File file4 = new File("${WORKSPACE}/Filtering_Services/validServicesRegular.properties")
 
-                        def inputFile = readProperties file: "${WORKSPACE}/Filtering_Services/inputfile1.properties"
+//                        def inputFile = readProperties file: "${WORKSPACE}/Filtering_Services/inputfile.properties"
+                        def inputFile = readProperties file: "${WORKSPACE}/Filtering_Services/errorinput.properties"
                         //println(inputFile.getClass())  // Hashmap
-                        def serviceFile = readFile file: "${WORKSPACE}/Filtering_Services/services-list"
-                        //println(serviceFile.getClass())  // String
-                        def schedulersFile = readFile file: "${WORKSPACE}/Filtering_Services/scheduler-list.txt"
+                        def serviceFile = readProperties file: "${WORKSPACE}/Filtering_Services/services-list"
+                        //println(serviceFile.getClass())  // Hashmap
+                        def schedulersFile = readProperties file: "${WORKSPACE}/Filtering_Services/scheduler-list.txt"
+                        //println(schedulersFile.getClass())  // Hashmap
 
                         inputFile.each { key, value ->
-                            if(serviceFile.contains(key))
-                                println("key found: ${key}") {
+                            if(serviceFile.containsKey(key)){
                                     validServices.add(key)
-                                    file1.append(key+"\n")
-                                }
-                                else{
-                                    invalidServices.add(key)
-                                    file2.append(key+"\n")
-                                }
+                                    file1.append(key+"="+value+"\n")
+                            }
+                            else{
+                                invalidServices.add(key)
+                                file2.append(key+"="+value+"\n")
+                            }
                         }
                         println("Valid Services:\n ${validServices}")
                         println("InValid Services:\n ${invalidServices}")
                         validServices.each{ item ->
-                            if(schedulersFile.contains(item)){
+                            if(schedulersFile.containsKey(item)){
                                 validSchedulers.add(item)
-                                file3.append(item+"\n")
                             }
                             else{
-                                println("${item} not found on scheduler-list")
+                                println("${item} not found on valid scheduler-list")
                             }
                         }
                         println("Valid Schedulers:\n ${validSchedulers}")
+                        def VALID_SERVICES_ALL = readProperties file:"${file1}"
+                        VALID_SERVICES_ALL.each{ key,value ->
+                            if(validSchedulers.contains(key)){
+                                file3.append(key+"="+value+"\n")
+                            }
+                            else{
+                                file4.append(key+"="+value+"\n")
+                            }
+                        }
 
-                    }//dir
+                     }//dir
                 }//script
             }//steps
         }//stage
--- updated-req.groovy
+++
@@ -1,6 +0,0 @@
-
-
-def file1 = readProperties("${WORKSPACE}/inputfile1.properties")  // Converts as a String
-println(file1.getClass())  // file1 is a string
-def inputlist = []
-println(file1.tokenize("="))
