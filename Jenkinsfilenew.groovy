8de037b4a67577282a021154e8d21766afd87931
snownguyen1011 <raviraja1988@hotmail.com>
2020-12-08 15:50:15 -0500

    base pipeline converted to scripted pipeline load path updated

--- Jenkinsfilenew.groovy
+++ Jenkinsfilenew.groovy
@@ -5,6 +5,6 @@ node{
               extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'Filtering_Services']],
               submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github', url: 'https://github.com/snownguyen1011/Groovy-Work.git']]])
     withEnv (['BASE_WORKSPACE=$WORKSPACE']) {
-        load 'Filtering_Services/Jenkinsfile.groovy'
+        load 'Filtering_Services/Filtering_Services/Jenkinsfile.groovy'
     }
 }
