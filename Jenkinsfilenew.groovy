269145de91b10ca32cd47acd0ac880ef085c216e
snownguyen1011 <raviraja1988@hotmail.com>
2020-12-08 15:38:52 -0500

    base pipeline converted to scripted pipeline Git URL updated

--- Jenkinsfilenew.groovy
+++ Jenkinsfilenew.groovy
@@ -3,7 +3,7 @@ node{
     checkout scm
     checkout([$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false,
               extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'Filtering_Services']],
-              submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github', url: 'url']]])
+              submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github', url: 'https://github.com/snownguyen1011/Groovy-Work.git']]])
     withEnv (['BASE_WORKSPACE=$WORKSPACE']) {
         load 'Filtering_Services/Jenkinsfile.groovy'
     }
