d39e534947b01d6b186b033ae0e6dd1a2c0328dc
snownguyen1011 <raviraja1988@hotmail.com>
2020-12-08 15:37:00 -0500

    base pipeline converted to scripted pipeline

--- Jenkinsfilenew.groovy
+++ Jenkinsfilenew.groovy
@@ -1,21 +1,10 @@
-pipeline {
-    agent any
-    //parameters {
-    //string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
-    //}
-    stages {
-        stage('Filter_Services') {
-            steps {
-                script {
-                    git credentialsId: 'github', url: 'https://github.com/snownguyen1011/Groovy-Work.git'
-//                    Def jenkinsFileNew = fileLoader.fromGit('Filtering_Services/Jenkinsfile.groovy', 'https://github.com/snownguyen1011/Groovy-Work.git', 'master', null, '')
-//                    jenkinsFileNew.start()
-                    dir("$WORKSPACE") {
-                        A = load 'Filtering_Services/Jenkinsfile.groovy'
-                        A.start()
-                    }//dir
-                }//script
-            }//steps
-        }//stage
-    }//stages
-}//pipeline
+node{
+    cleanWs()
+    checkout scm
+    checkout([$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false,
+              extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'Filtering_Services']],
+              submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github', url: 'url']]])
+    withEnv (['BASE_WORKSPACE=$WORKSPACE']) {
+        load 'Filtering_Services/Jenkinsfile.groovy'
+    }
+}
